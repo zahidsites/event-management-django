e1e945796aed25c7f75a20eb5fb1a94fd48ee116
zahid <zahidsites@gmail.com>
2025-07-24 05:50:51 +0600

    adding all models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from tasks.models import Category, Participant, Event
 admin.site.register(Category)
+admin.site.register(Participant)
+admin.site.register(Event)
 
 # Register your models here.
